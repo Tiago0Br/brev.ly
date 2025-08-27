f8617d9b05a601b237fcdad40652492a40ff9537
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-08-26 22:14:40 -0300

    feature: criação da base da tela inicial
    - Foi criada a interface base da tela inicial contendo o formulário de cadastro de links
    - Foram adicionadas as cores do botão de criar link na paleta de cores

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,42 @@
 export function App() {
-  return <h1>Brev.ly</h1>
+	return (
+		<main className="mt-20">
+			<form className="mx-auto w-[380px] h-[340px] bg-gray-100 rounded-lg p-8 flex flex-col gap-6">
+				<h2 className="text-xl text-gray-600 font-bold">Novo link</h2>
+
+				<div className="flex flex-col gap-4">
+					<div className="flex flex-col gap-2">
+						<label htmlFor="originalLink" className="text-xs uppercase text-gray-500">
+							Link original
+						</label>
+						<input
+							type="text"
+							id="originalLink"
+							placeholder="www.exemplo.com.br"
+							className="border border-gray-300 rounded-md p-2 placeholder:text-sm"
+						/>
+					</div>
+
+					<div className="flex flex-col gap-2">
+						<label htmlFor="shortLink" className="text-xs uppercase text-gray-500">
+							Link encurtado
+						</label>
+						<input
+							type="text"
+							id="shortLink"
+							placeholder="brev.ly/"
+							className="border border-gray-300 rounded-md p-2 placeholder:text-sm"
+						/>
+					</div>
+				</div>
+
+				<button
+					type="submit"
+					className="w-full bg-blue-base text-white rounded-md p-2 cursor-pointer hover:bg-blue-dark transition-colors"
+				>
+					Salvar link
+				</button>
+			</form>
+		</main>
+	)
 }
